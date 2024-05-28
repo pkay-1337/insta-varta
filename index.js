452e13e45a5f121de82bce58214a5b691ea8f612
pkay-1337 <kumarpramod66476@gmail.com>
2024-05-28 19:57:43 +0530

    smthing smthing

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 var express = require('express')
+const path = require('path');
+
+const mariadb = require('mariadb');
+
+
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'pk',
+  password: 'pk',
+  database: 'insta',
+  connectionLimit: 20
+});
+
 var app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.static('public'))
+
+app.get('/login', (req, res) => {
+    console.log(path.join(path.dirname(require.main.filename)))
+    res.sendFile(path.join(path.dirname(require.main.filename),"public/html/login.html"))
+})
+app.post('/login', (req, res) => {
+  console.log(req);
+  res.redirect('/login');
 })
 
 app.listen(port, () => {
