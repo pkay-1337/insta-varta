f518f12cef9d34d1b5ad4647d5a1c112c79bce28
pkay-1337 <kumarpramod66476@gmail.com>
2024-05-28 18:08:30 +0530

    Cleanup, I DONT LIKE BLOAT

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var express = require('express')
 var app = express()
+const port = 443
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
