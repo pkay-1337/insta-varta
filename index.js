31b86111ac5601d16c11437071aa9ba551f7292c
Pramod <kumarpramod66476@gmail.com>
2024-07-07 07:32:01 +0000

    on arch

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ const { app, server } = require("./js/ws");
 //(async () => {conn = await con.getConnection()})();
 
 app.get("/", (req, res) => {
-	init(req, res);
+  init(req, res);
 });
 app.get("/login", (_req, res, _next) => {
-	res.status(299).sendFile(path.join(__dirname, "public/html/login.html"));
+  res.status(299).sendFile(path.join(__dirname, "public/html/login.html"));
 });
 /*
 app.get("/home", (req, res) => {
@@ -38,35 +38,35 @@ app.get("/home", (req, res) => {
 });
 */
 app.get("/profile", (req, res) => {
-	profile(req, res);
-	//get(req, res, "../public/html/profile.html");
+  profile(req, res);
+  //get(req, res, "../public/html/profile.html");
 });
 app.get("/edit", (req, res) => {
-	get(req, res, "../public/html/edit.html");
+  get(req, res, "../public/html/edit.html");
 });
 app.get("/profilePic", (req, res) => {
-	get(req, res, "profile");
+  get(req, res, "profile");
 });
 app.get("/upload", (req, res) => {
-	get(req, res, "../public/html/upload.html");
+  get(req, res, "../public/html/upload.html");
 });
 app.get("/myphotos", (req, res) => getphotos(req, res, "$"));
 app.get("/userphotos", (req, res) => getphotos(req, res, 0));
 app.get("/photos", (req, res) => photos(req, res));
 app.get("/photo", (req, res) => {
-	get(req, res, "../uploads/" + req.query["p"]);
+  get(req, res, "../uploads/" + req.query["p"]);
 });
 app.get("/start", (req, res) => {
-	get(req, res, "../public/html/start.html");
+  get(req, res, "../public/html/start.html");
 });
 app.get("/search", (req, res) => {
-	get(req, res, "../public/html/search.html");
+  get(req, res, "../public/html/search.html");
 });
 app.get("/user", (req, res) => {
-	get(req, res, "../public/html/user.html");
+  get(req, res, "../public/html/user.html");
 });
 app.get("/getchatusers", (req, res) => {
-	getchatusers(req, res);
+  getchatusers(req, res);
 });
 app.get("/follow", (req, res) => follow(req, res));
 app.get("/doifollow", (req, res) => doifollow(req, res));
@@ -76,19 +76,19 @@ app.get("/getchat", (req, res) => getchat(req, res));
 app.post("/register", (req, res) => reg.register(req, res));
 app.post("/login", (req, res) => log.login(req, res));
 app.post("/edit", (req, res) => {
-	edit(req, res);
+  edit(req, res);
 });
 app.post("/upload", (req, res) => {
-	upload(req, res);
+  upload(req, res);
 });
 app.post("/search", (req, res) => {
-	search(req, res);
+  search(req, res);
 });
 app.post("/send", (req, res) => {
-	send(req, res);
+  send(req, res);
 });
 
 const port = 443;
 server.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
