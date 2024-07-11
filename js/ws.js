825d3fcc6525ba08e18829e44281acff64b9b69b
Pramod <kumarpramod66476@gmail.com>
2024-07-11 13:50:33 +0530

    on arch

--- js/ws.js
+++ js/ws.js
@@ -10,34 +10,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const options = {
-	key: fs.readFileSync("certs/key.pem"),
-	cert: fs.readFileSync("certs/cert.pem"),
+  key: fs.readFileSync("certs/key.pem"),
+  cert: fs.readFileSync("certs/cert.pem"),
 };
 const server = https.createServer(options, app);
 
 const wss = new WebSocket.Server({ server });
 socs = {};
 wss.on("connection", (ws, request) => {
-	console.log("New client connected");
-	const parsedUrl = url.parse(request.url, true);
-	let path = parsedUrl.pathname;
-	path = path.slice(1);
-	//console.log(path);
-	// Message event handler
-	socs[path] = ws;
-	console.log("socs path : " + path);
-	ws.on("message", (message) => {
-		const parsedUrl = url.parse(request.url, true);
-		let path = parsedUrl.pathname;
-		path = path.slice(1);
-		console.log(`Received from ${path} : ${message}`);
-		//socs[path].send(`${message}`);
-		socs[path].send(JSON.stringify({ success: "Message Recieved" }));
-	});
+  console.log("New client connected");
+  const parsedUrl = url.parse(request.url, true);
+  let path = parsedUrl.pathname;
+  path = path.slice(1);
+  //console.log(path);
+  // Message event handler
+  socs[path] = ws;
+  console.log("socs path : " + path);
+  ws.on("message", (message) => {
+    const parsedUrl = url.parse(request.url, true);
+    let path = parsedUrl.pathname;
+    path = path.slice(1);
+    console.log(`Received from ${path} : ${message}`);
+    //socs[path].send(`${message}`);
+    socs[path].send(JSON.stringify({ success: "Message Received" }));
+  });
 
-	// Close event handler
-	ws.on("close", () => {
-		console.log("Client disconnected");
-	});
+  // Close event handler
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
 });
 module.exports = { app, wss, socs, server };
